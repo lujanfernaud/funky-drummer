59da5ab94d6e681ca4a81040bdfaf78b10a4224d
lujanfernaud <lujanfernaud@gmail.com>
2016-12-15 08:11:33 +0000

    Change key color on key pressed

--- funky-drummer.js
+++ funky-drummer.js
@@ -1,3 +1,14 @@
-window.addEventListener('keydown', function(e) {
-  console.log(e)
+window.addEventListener('keydown', function(k) {
+  var key = document.querySelector(`div[data-key="${k.keyCode}"]`);
+  if(!key) return;
+
+  key.classList.add('key-active');
+  console.log(key);
+});
+
+window.addEventListener('keyup', function(k) {
+  var key = document.querySelector(`div[data-key="${k.keyCode}"`);
+  if(!key) return;
+
+  key.classList.remove('key-active');
 });
